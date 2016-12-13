97e02903efd9f86ec56ae49961f1621eb19b7511
italoal1 <italo.alves.jp@outlook.com>
2016-12-13 06:53:12 -0300

    alteração na função exibe_matriz que agora passa a apenas exibir a matriz, a gravação do array estático fica por conta de grava_valor()

--- lib/libio.h
+++ lib/libio.h
@@ -12,14 +12,13 @@
 	#define limpar_console() system("clear")
 #endif
 
-#define GRAVAR 0
-#define EXIBIR 1
 #define IGNORE 5
 #define SUCESSO 0
 #define FRACASSO 1
 
 void menu();
-void exibe_matriz(int valor, int x, int y, int interruptor);
+void exibe_matriz(int valor, int x, int y);
+void grava_valor(int valor, int x, int y);
 int recebe_coord(int *x, int *y);
 void credito();
 void exibe_rank();
