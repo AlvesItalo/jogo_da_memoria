32e6d43a6af62179e80c81cc1f47e5b79124f197
italoal1 <italo.alves.jp@outlook.com>
2016-12-13 02:23:01 -0300

    mais prototipos

--- lib/libgame.h
+++ lib/libgame.h
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "libio.h"
 
 int um_jogador();
 int dois_jogadores();
 int validar_coord();
-
 void preeche_matriz(int *matriz);
 
 #endif
---
+++ memory_game.c
@@ -0,0 +1,8 @@
+#include "lib/libio.h"
+#include "lib/libgame.h"
+#include "lib/librank.h"
+
+int main(){
+	
+	return 0;
+}
