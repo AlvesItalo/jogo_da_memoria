83b9323ac67dc7a113d24e078f463b8b3bc96b91
matheus_hensley <matheushensley_25@hotmail.com>
2016-12-13 16:38:34 -0300

    Funcao grava_rank

--- lib/librank.c
+++ lib/librank.c
@@ -1,1 +1,13 @@
 #include "librank.h"
+
+void grava_ranking(t_jogador player){
+    int tam;
+
+    FILE *arq = fopen("teste.bin", "ab+");/*Mudar nome do arquivo gerado e passar caminho*/
+    rewind(arq);
+    fseek(arq, 0, SEEK_SET);
+    tam = strlen(player.nome);
+    fwrite(&player, sizeof(player.pontuacao) + tam, 1, arq);
+    fflush(arq);
+    fclose(arq);
+}
